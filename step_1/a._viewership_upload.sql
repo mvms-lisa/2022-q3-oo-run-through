da59ce267ba6284a9840e420ba7a9806e52a94b6
mvms-lisa <112023312+mvms-lisa@users.noreply.github.com>
2023-01-25 12:43:12 -0500

    added powr

--- step_1/a._viewership_upload.sql
+++ step_1/a._viewership_upload.sql
@@ -1,5 +1,3 @@
--- powr viewership on hold -- sept missing
-
 -- gam_data
     -- fields to update: 
         --  filename, year, quarter, pattern (replace where there is an 'X')
@@ -41,6 +39,43 @@
         from @oo_revenue t) pattern='.*spotx_revenue_q3_2022.*' file_format = nosey_viewership 
         ON_ERROR=SKIP_FILE FORCE=TRUE;
         
+-- powr
+    -- copy into from register POWR     
+       copy into powr_viewership(
+        title, 
+        type, 
+        channel, 
+        views, 
+        watch_time_seconds, 
+        average_watch_time_seconds, 
+        platform, 
+        geo, 
+        year_month_day,
+        ref_id,
+        content_provider,
+        series,
+        quarter,
+        year,
+        filename
+        )   
+        from (select t.$1, t.$2, t.$3, to_number(REPLACE(t.$4, ','), 12, 2), to_decimal(REPLACE(t.$5,  ','), 12, 2), to_number(REPLACE(REPLACE(t.$6, '-', ''), ','), 16, 6), t.$7, t.$8, t.$9, t.$10, t.$11, t.$12, 'q3', 2022,  'powr_viewership_register_q3_2022.csv'
+        from @oo_viewership t) pattern='.*powr_viewership_register_q3_2022.*' file_format = nosey_viewership 
+        ON_ERROR=SKIP_FILE FORCE=TRUE;
+    
+    -- powr_viewership - update device and department
+    update powr_viewership p
+        -- set device id column to val in query, set dept id to val in query
+        set p.device_id = q.devid, p.department_id = q.depid
+        from 
+        (
+            -- query to match viewership record to the device
+            select p.id as qid, d.device_id as devid, d.department_id as depid from powr_viewership p
+            join dictionary.public.devices d on (d.entry = p.platform)
+            where year = 2022 and quarter = 'q3'
+        ) q
+        -- update where the record id matches the record id in query
+        where p.id = q.qid
+
         
 -- AdX with Description - To Paste Into CSV for Revenue Upload (or to manually insert)
 insert into revenue(year_month_day, amount, pay_partner, title, description, impressions, department_id, month, quarter, year, filename)
