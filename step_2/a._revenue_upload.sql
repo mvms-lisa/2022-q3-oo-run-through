76b8c331e103cba75e9a4c24f1d387a3faf543c1
Lisa <lisareyes@mvmediasales.com>
2023-01-25 12:48:49 -0500

    revenue statements added

---
+++ step_2/a._revenue_upload.sql
@@ -0,0 +1,27 @@
+--Revenue Upload
+    copy into revenue(year_month_day, amount, pay_partner, title, type, description, impressions, department, department_id, cpm, quarter, year, month, label, filename)
+    from (select t.$1, to_number(REPLACE(REPLACE(t.$2, '$', ''), ','), 12, 2), t.$3, t.$4, t.$5, t.$6, to_number(REPLACE(t.$7, ','),12, 0), t.$8, t.$9, to_number(REPLACE(t.$10, ','),6, 2), t.$11, t.$12, t.$13,'Revenue', 'revenue_q3_2022'
+    from @oo_revenue t) pattern='.*revenue_q3_2022.*' file_format = nosey_viewership 
+    ON_ERROR=SKIP_FILE FORCE=TRUE;
+
+    -- SpotX with Description - To Paste Into CSV for Revenue Upload (or to manually insert)
+    insert into revenue(year_month_day, amount, pay_partner, title, description, impressions, department_id, month, quarter, year, filename)
+    select year_month_day, sum(gross_revenue) as revenue, 'spotx' as pay_partner, 'SpotX' as title, channel_name as description, sum(impressions) as impressions, department_id, month, quarter, year, 'Manual Insert' as filename
+    from spotx where year = 2022 and quarter = 'q3' and channel_name not like '%Tegna%' and gross_revenue != 0
+    group by channel_name, year_month_day, quarter, year, department_id, month
+    order by channel_name
+    
+    -- AdX with Description - To Paste Into CSV for Revenue Upload (or to manually insert)
+    insert into revenue(year_month_day, amount, pay_partner, title, description, impressions, department_id, month, quarter, year, filename)
+    select year_month_day, sum(ad_exchange_revenue) as revenue, 'adx' as pay_partner, advertiser as title, ad_unit as description, sum(total_impressions) as impressions, department_id, month, quarter, year, 'Manual Insert' as filename
+    from gam_data where year = 2022 and quarter = 'q3' and advertiser = 'AdX'
+    group by ad_unit, year_month_day, quarter, year, department_id, advertiser, month
+    order by ad_unit
+    
+    --9 Media Online
+    insert into revenue(year_month_day, amount, pay_partner, title, description, impressions, department_id, month, quarter, year, filename, label)
+    select year_month_day, sum(gross_revenue) as revenue, '9mediaonline' as pay_partner, '9MediaOnline' as title, channel_name as description, 
+    sum(impressions) as impressions, department_id, month, quarter, year, 'Manual Insert' as filename, 'Revenue' as label
+    from spotx where year = 2022 and quarter = 'q3' and deal_name like '%9 Media%'
+    group by channel_name, year_month_day, quarter, year, department_id, month
+    order by channel_name
